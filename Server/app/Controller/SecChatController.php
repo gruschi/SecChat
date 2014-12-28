8b160f3f19058aa7c97bfb7c0a68fa3e35d18d0a
Manuel Gruschka <manuel.gruschka@bg-world.de>
2014-12-28 11:40:42 +0100

    Einiges Sicherer Gemacht, viele Kleine Fehler verbessert.

--- Server/app/Controller/SecChatController.php
+++ Server/app/Controller/SecChatController.php
@@ -4,90 +4,178 @@ class SecChatController extends AppController {
 	
 public function beforeFilter() {
 		parent::beforeFilter();
- 		$this->Auth->allow('connect', 'service', 'serviceConnect', 'destroyChatSession');
+
+		$this->Auth->allow('connect', 'service', 'serviceConnect', 'destroyChatSession');
 	}
 	
+	/**
+	 * @sicher
+	 */
 	public function index(){		
-		$this->layout = "ajax";
+		if($this->request->is("post")){
+			$this->layout = "ajax";
+		}else{
+			
+		}
 	}
 	
+	/**
+	 * Um eine Verbindung mit einem anderen Client herzustellen.
+	 * @param string $type (contact/group)
+	 * @sicher
+	 */
 	public function connect($type=null){
-		$this->loadModel("Connection");
 		
-		$result = null;		
-		if($type == "contact"){			
-			$this->Connection->create();			
-			if($this->Connection->save($this->request->data)){				
+		if($this->request->is("post")){
+			$this->loadModel("Connection");
+			
+			$result = null;
+			if($type == "contact"){
+				$this->Connection->create();
+				if($this->Connection->save($this->request->data)){
+					$result = array("chatSessionId" => $this->Connection->id);
+				}else{
+					debug($this->Connection->invalidFields());
+				}
+			}
+			
+			$this->set("result", json_encode($result));
+			
+			$this->layout = "ajax";
+		}else{
+			$this->set("result", __("No Access"));
+		}
+		
+	}
+	
+	/**
+	 * Gegenstück zu connect, was alle 5 Sek. aufgerufen wird:
+	 * Connecter muss nach connect service pullen um Receiver-Daten zu bekommen.
+	 * @sicher
+	 */
+	public function service(){
+		if($this->request->is("post")){
+			$this->loadModel("Connection");
+			$this->loadModel("User");
+						
+			$result = null;
+			if(isset($this->request->data["User"]["sessionId"])){
+				//GetUserId
+				$objUser = $this->User->find("first", array("conditions" => array("User.sessionId" => $this->request->data["User"]["sessionId"])));
+					
+				$result = $this->Connection->find('all',
+						array(
+								"conditions" =>
+								array(	"Connection.receiverId" => intval($objUser["User"]["id"]),										
+										"Connection.send" => false),
+								"fields" => array(
+										"Connection.id",
+										"Connection.senderId",
+										"Connection.alias",
+										"Connection.receiverPin",
+										"Connection.pubKey"
+								),
+								"limit" => 1
+						)
+				);									
+					
+				//Flag setzen in Connection
+				if(!empty($result)){					
+					$cR = count($result);
+					for($i = 0; $i < $cR; $i++){
+						if(isset($result[$i]["Connection"]["id"])){
+							$this->Connection->updateAll(
+									array("Connection.send" => true),
+									array("Connection.id" => $result[$i]["Connection"]["id"]));
+				
+							unset($result[$i]["Connection"]["id"]);//ID löschen um Verwirrung zu vermeiden.
+						}
+					}
+				}
+			}	
+			
+			$return = array("chatSession" => $result);
+			
+			$this->set("result", json_encode($return));			
+ 	 		$this->layout = "ajax";
+		}else{
+			$this->set("result", __("No Access"));
+		}
+	}
+	
+	/**
+	 * Gegenstück zu Connect
+	 * @sicher
+	 */
+	public function serviceConnect(){
+		
+		if($this->request->is("post")){
+			$this->loadModel("Connection");
+			
+			//Tut das gleiche wie Contact		
+			$this->Connection->create();
+			if($this->Connection->save($this->request->data)){
 				$result = array("chatSessionId" => $this->Connection->id);
 			}else{
- 				debug($this->Connection->invalidFields()); 
+				debug($this->Connection->invalidFields());
 			}
-		}
-		
-		$this->set("result", json_encode($result));
-		
-		$this->layout = "ajax";
-	}
-	
-	public function service(){
-		$this->loadModel("Connection");
-		$this->loadModel("User");
-		
-		//Get Incoming Connections
-//  		echo "<br>UserId: ".$this->Auth->user("user");
-		
-		//GetUserId		
-		$objUser = $this->User->find("first", array("conditions" => array("User.sessionId" => $this->request->data["Connection"]["sessionId"])));		
-		
-		$result = $this->Connection->find('all', 
-					array(
-							"conditions" => 
-								array("Connection.receiverId" => intval($objUser["User"]["id"]), "Connection.receiverPin IS NOT NULL"),
-							"fields" => array(
-								"Connection.senderId", "Connection.alias", "Connection.receiverPin", "Connection.PubKey"
-							)
-					)
-				);			
-		
-		$return = array("chatSession" => $result);
-		
-		$this->set("result", json_encode($return));
-
- 		$this->layout = "ajax";
-	}
-	
-	public function serviceConnect(){
-		$this->loadModel("Connection");
-		
-		//Tut das gleiche wie Contact
-		$this->Connection->create();
-		if($this->Connection->save($this->request->data)){
-			$result = array("chatSessionId" => $this->Connection->id);
-		}else{
-			debug($this->Connection->invalidFields());
-		}
-		
-		$this->set("result", json_encode($result));
-		
-		$this->layout = "ajax";
-		
-	}
-	
-	public function destroyChatSession(){
-		$this->loadModel("Connection");
-		$return = false;
-		if(isset($this->request->data["Connection"]["id"]) && isset($this->request->data["User"]["sessionId"])){
-			//TODO Teste ob SessionId zur senderId passt.			
-			$this->Connection->delete($this->request->data('Connection.id'));
 			
-			$return = true;
+			$this->set("result", json_encode($result));
+			
+			$this->layout = "ajax";
+		}else{
+			$this->set("result", __("No Access"));
+		}				
+	}
+	
+	/**
+	 * Zerstört eine ChatSession
+	 * @sicher
+	 */
+	public function destroyChatSession(){
+		if($this->request->is("post")){
+			$this->loadModel("Connection");
+			$this->loadModel("User");
+			
+			$return = false;
+			if(isset($this->request->data["Connection"]["id"]) && isset($this->request->data["User"]["sessionId"])){
+				
+				$objUser = $this->User->find("first", array("conditions" => array("User.sessionId" => $this->request->data("User.sessionId"))));
+				
+				$curConnection = $this->Connection->find("first", 
+						array("conditions" => array(
+								"Connection.id" => $this->request->data('Connection.id'),
+								"OR" => array(
+									"Connection.senderId" => $objUser["User"]["id"],
+									"Connection.receiverId" => $objUser["User"]["id"]
+								)
+				)));				
+				
+				if(!empty($curConnection)){
+					//Lösche auch entgegengesetze Richtung					
+					$altConnection = $this->Connection->find("first", array("conditions" => 
+							array(
+									"Connection.senderId" => $curConnection["Connection"]["receiverId"],
+									"Connection.receiverId" => $curConnection["Connection"]["senderId"]
+					)));
+					
+					$this->Connection->delete($altConnection["Connection"]["id"]);
+					
+					$this->Connection->delete($this->request->data('Connection.id'));
+					$return = true;
+				}						
+									
+			}
+			
+			$result = array("chatDisconnected" => $return);
+			
+			$this->set("result", json_encode($result));
+			
+			$this->layout = "ajax";
+		}else{
+			$this->set("result", __("No Access"));
 		}
 		
-		$result = array("chatDisconnected" => $return);
-		
-		$this->set("result", json_encode($result));
-		
-		$this->layout = "ajax";
 	}
 }
 
